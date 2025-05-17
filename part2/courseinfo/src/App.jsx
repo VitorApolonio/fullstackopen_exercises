ee3cdc6165686b5a1c2705d603b396b92d74c563
VitorApolonio <vitorapolonio74@gmail.com>
2025-05-17 17:30:08 -0300

    courseinfo step 9

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -34,34 +34,60 @@ const Total = ({ parts }) => {
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3,
-      },
-      {
-        name: 'Fundamentals of Group Theory',
-        exercises: 342,
-        id: 4,
-      },
-    ],
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: 'Fundamentals of Group Theory',
+          exercises: 342,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: 'Learn OpenGL',
+      id: 2,
+      parts: [
+        {
+          name: 'Fundamentals of C++',
+          exercises: 932,
+          id: 1,
+        },
+        {
+          name: 'Basics of Linear Algebra',
+          exercises: 1,
+          id: 2,
+        },
+        {
+          name: 'Displaying hello world',
+          exercises: 3213,
+          id: 3,
+        },
+      ],
+    },
+  ]
 
   return (
-    <Course course={course} />
+    <div>
+      {courses.map(c => <Course course={c} />)}
+    </div>
   )
 }
 
