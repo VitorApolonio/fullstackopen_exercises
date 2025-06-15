4c2cea0b63278324da2e7f654035260105d75646
VitorApolonio <vitorapolonio74@gmail.com>
2025-06-15 16:45:05 -0300

    phonebook backend step 8

--- part3/pb_backend/index.js
+++ part3/pb_backend/index.js
@@ -4,7 +4,9 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :json-data'))
+
+morgan.token('json-data', (req, res) => JSON.stringify(req.body))
 
 let persons = [
   { 
